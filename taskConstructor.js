f622a87455a219ab45b8d6372bf133fb0f3dcce1
Nikolay Ignatiev <noilgt@gmail.com>
2016-02-09 12:34:14 +0300

    Added prototype functions

--- taskConstructor.js
+++ taskConstructor.js
@@ -2,24 +2,31 @@
 var Task = function(name) {
 	this.name = name;
 	this.completed = false;
-	// method for change task state
-	this.completed = function() {
-		console.log('completing task: ' + this.name);
-		this.completed = true;
-	}
-	// method for saving task
-	this.save = function() {
-		console.log('saving Task.. ' + this.name);
-	}
 }
 
+// Encapsulate methods in prototype
+Task.prototype.complete = function() {
+	console.log('completing task: ' + this.name);
+	this.completed = true;
+}
+
+Task.prototype.save = function() {
+	console.log('Saving task...');
+}
+
+Task.prototype.printInfo = function() {
+	console.log('Task name: ' + this.name + ' ' + '| Task complete state: ' + this.completed );
+}
 // Create sample tasks
 var task1 = new Task('Sample of task construction');
 var task2 = new Task('Sample of task for module');
 var task3 = new Task('Sample of task for prototype');
 // Change state
-task1.completed();
+task1.complete();
 task1.save();
 task2.save();
 task3.save();
 
+task1.printInfo();
+task2.printInfo();
+task3.printInfo();
